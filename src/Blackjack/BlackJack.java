27ce70ed1fbe8813ba9a32e3f12b58b446650b6f
houssamcha <157011410+houssamcha@users.noreply.github.com>
2024-02-19 16:40:34 +0100

    Update BlackJack.java
    Fixing the Class main

--- src/Blackjack/BlackJack.java
+++ src/Blackjack/BlackJack.java
@@ -3,19 +3,24 @@ package Blackjack;
 import java.util.Scanner;
 
 public class BlackJack {
-
+    static La_main laMain = new La_main();
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int choice;
-        System.out.println("╔═══════════════════════════╗");
-        System.out.println("║        Cool Menu          ║");
-        System.out.println("╠═══════════════════════════╣");
-        System.out.println("║  1. Option 1              ║");
-        System.out.println("║  2. Option 2              ║");
-        System.out.println("║  3. Option 3              ║");
-        System.out.println("║  4. Exit                  ║");
-        System.out.println("╚═══════════════════════════╝");
+        System.out.println("  BIENVENUE AU BLACKJACK   ");
+        System.out.println("  1. Commencer             ");
+        System.out.println("  2. Exit                  ");
         System.out.print("Enter your choice: ");
         choice = scanner.nextInt();
+        switch (choice){
+            case 1:
+                laMain.Distribuer_cartes();
+                break;
+            case 2 :
+
+                break;
+            default:
+                System.out.println("Entrer 1 ou 2");
+        }
     }
 }
